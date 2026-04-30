5988be6a92c946bda0145e6737b138644616fe55
germano210 <2024020049@aluno.osorio.ifrs.edu.br>
2026-04-30 20:37:19 -0300

    Criado classe2 conforme solicitado em Lab4

---
+++ Classe2.java
@@ -0,0 +1,6 @@
+// código original
+     public class Classe2 {
+         public static void main(String[] args) {
+             System.out.println("classe2 do another_feature");
+         }
+     }
