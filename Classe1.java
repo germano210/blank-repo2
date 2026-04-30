7564cab5e04ca4829493da883cc7ad319af8126f
germano210 <2024020049@aluno.osorio.ifrs.edu.br>
2026-04-30 20:35:40 -0300

    Alterado codigo do classe1 conforme solicitado que seja feito no new_feature

--- Classe1.java
+++ Classe1.java
@@ -1,6 +1,6 @@
-// código original
-     public class classe1 {
+// mudanças no código
+     public class Classe1 {
          public static void main(String[] args) {
-             System.out.println("Versão inicial");
+             System.out.println("Modificado no branch new_feature");
          }
      }
