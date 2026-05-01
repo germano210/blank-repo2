250021bc8c00c510f5070fc37e279baf274f6523
germano210 <2024020049@aluno.osorio.ifrs.edu.br>
2026-04-30 21:23:10 -0300

    Alterado conforme solicitado para o branch new_featureB

--- Classe3.java
+++ Classe3.java
@@ -1,6 +1,6 @@
- // codigo original
-   public class Classe3 {
-       public static void main(String[] args) {
-           System.out.println("Versão base");
-       }
-   }
+// código original
+       public class Classe3 {
+           public static void main(String[] args) {
+                System.out.println("Modificado no new_featureB");
+           }
+        }
